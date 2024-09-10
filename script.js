8c307e6ccaef117deba6b6c5e99b9a762953649c
yuri-italo <yuri.italo94@gmail.com>
2024-09-09 21:09:10 -0300

    feat: add hover effect to the grid

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ for (let i = 0; i < divQuantity; i++) {
   div.className = "square";
   container.appendChild(div);
 }
+
+const squares = document.querySelectorAll(".square");
+Array.from(squares).forEach((square) => {
+  square.addEventListener("mouseenter", handleMouseEnter);
+});
+
+function handleMouseEnter(event) {
+  const target = event.target;
+  target.style.backgroundColor = "black";
+}
