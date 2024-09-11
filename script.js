a91b2a875362fe31a6060ecdb10787de44fb1fb2
yuri-italo <yuri.italo94@gmail.com>
2024-09-11 20:02:23 -0300

    fix: add createGrid function

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const container = document.querySelector(".container");
-const divQuantity = 16 * 16;
 
-for (let i = 0; i < divQuantity; i++) {
-  const div = document.createElement("div");
-  div.className = "square";
-  container.appendChild(div);
-}
+createGrid(16);
 
 const squares = document.querySelectorAll(".square");
 Array.from(squares).forEach((square) => {
   square.addEventListener("mouseenter", handleMouseEnter);
 });
+
+function createGrid(sideSize) {
+  const divQuantity = sideSize * sideSize;
+  for (let i = 0; i < divQuantity; i++) {
+    const div = document.createElement("div");
+    div.className = "square";
+    container.appendChild(div);
+  }
+}
 
 function handleMouseEnter(event) {
   const target = event.target;
